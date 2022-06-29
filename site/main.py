0a922f1590e6aad3d469c2ea4a6e8341f416c71b
lbeete <96639895+lbeete@users.noreply.github.com>
2022-06-29 17:32:19 -0400

    Add files via upload

---
+++ site/main.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__, template_folder='Templates')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/api')
+def rest():
+    return jsonify({"xd": "hola"})
+
+if __name__ == '__main__':
+    print('Done!')
+    app.run(debug=True)
