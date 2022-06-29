81fa33da8d314e025cf8bb9bbe021a1599bee4eb
lbeete <96639895+lbeete@users.noreply.github.com>
2022-06-29 17:33:35 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, jsonify
 
 app = Flask(__name__, template_folder='Templates')
 
+#a
+
 @app.route('/')
 def home():
     return render_template('index.html')
